0b80bd6b1f2339ec684865dbc930af7488fa2557
Al-E-00 <mazzellaalessio6@gmail.com>
2022-05-23 17:52:33 +0200

    var outside

--- dice/js/action-dice.js
+++ dice/js/action-dice.js
@@ -36,12 +36,13 @@ if (pcRandomNum < userRandomNum) {
 
 //let's do 6 match and print for each match the result 
 
+let pcRandomNum = Math.floor(Math.random() * 6) + 1;
+let userRandomNum = Math.floor(Math.random() * 6) + 1;
 for (let i = 0; i < 6; i++) {
-    const pcRandomNum = Math.floor(Math.random() * 6) + 1;
-    const userRandomNum = Math.floor(Math.random() * 6) + 1;
     document.querySelector(".pcResults").innerHTML += (`<li class="my-3">${pcRandomNum}</li>`)
     document.querySelector(".userResult").innerHTML += (`<li class="my-3">${userRandomNum}</li>`)
 
-    const sumPcRandomNum = pcRandomNum[i] + pcRandomNum [i];
-    console.log(sumPcRandomNum);
 }
+
+const sumPcRandomNum = (pcRandomNum + pcRandomNum);
+console.log(sumPcRandomNum);
