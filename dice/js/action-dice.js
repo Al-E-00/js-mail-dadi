0cce0daac5da0b1034dd3ea992f356580c12afc4
Al-E-00 <mazzellaalessio6@gmail.com>
2022-05-23 17:42:10 +0200

    dice v 2.0 (bonus)

--- dice/js/action-dice.js
+++ dice/js/action-dice.js
@@ -11,25 +11,36 @@
 // 1. generate casual num from 1 to 6 two times, one from the computer
 // and the other from the player
 
-const pcRandomNum = Math.floor(Math.random() * 6) + 1;
+/* const pcRandomNum = Math.floor(Math.random() * 6) + 1;
 
 console.log(pcRandomNum);
 
 const userRandomNum = Math.floor(Math.random() * 6) + 1;
 
-console.log(userRandomNum);
+console.log(userRandomNum); */
 
 //2. we need to compare the two numbers to find who have the highest
 
-if (pcRandomNum == userRandomNum) {
+/* if (pcRandomNum == userRandomNum) {
     console.log ("Game ended in a draw");
-    document.getElementById("result").innerHTML = ("Game ended in a draw");
 }
 if (pcRandomNum > userRandomNum) {
     console.log ("Pc have won");
-    document.getElementById("result").innerHTML = ("Pc have won");
 } 
 if (pcRandomNum < userRandomNum) {
     console.log ("User have won");
-    document.getElementById("result").innerHTML = ("User have won");
+}
+ */
+
+// bonus
+
+//let's do 6 match and print for each match the result 
+
+for (let i = 0; i < 6; i++) {
+    const pcRandomNum = Math.floor(Math.random() * 6) + 1;
+    const userRandomNum = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".pcResults").innerHTML += (`<li class="my-3">${pcRandomNum}</li>`)
+    document.querySelector(".userResult").innerHTML += (`<li class="my-3">${userRandomNum}</li>`)
+
+    const sumPcRandomNum = pcRandomNum +
 }
